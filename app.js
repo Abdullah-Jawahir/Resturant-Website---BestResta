b9dad75e27c5af1202a948cee813c80ee205733e
Abdullah-Jawahir <mjabdullah33@gmail.com>
2023-09-17 21:04:07 +0530

    Final Changes

--- app.js
+++ app.js
@@ -3,3 +3,10 @@ const setVideoUrl = (theUrl) => {
     const video = document.querySelector("#video-carousel video");
     video.src = theUrl;
 }
+
+
+setInterval(() => {
+    const slider = document.querySelector('.food-categories-slider');
+    const firstCard = slider.querySelector('.food-category-card:first-child');
+    slider.appendChild(firstCard);
+}, 4000);
